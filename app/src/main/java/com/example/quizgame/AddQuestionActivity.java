87d37db6877206ce622c0f4e35ac68d1aa743d43
LAPTOP-0AVI4QJP\sunny <xxx@xxx.com>
2017-04-09 14:42:16 -0500

    Added Add Question activity

--- app/src/main/java/com/example/quizgame/AddQuestionActivity.java
+++ app/src/main/java/com/example/quizgame/AddQuestionActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class AddQuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_question);
     }
 }
